c0e7bed668a615a5f82c5d4ff5336dbffb481d68
nadirbashir <nadirbashir@intovative.com>
2025-08-01 20:00:15 +0500

    initialize database

--- src/database/core.py
+++ src/database/core.py
@@ -1,3 +1,4 @@
+from sqlmodel import SQLModel
 from typing import Annotated
 from fastapi import Depends
 from sqlmodel import create_engine, Session
@@ -5,6 +6,9 @@ from sqlmodel import create_engine, Session
 DATABASE_URL: str = "sqlite:///./test.db"
 
 engine = create_engine(DATABASE_URL)
+
+def init_db():
+    SQLModel.metadata.create_all(engine)
 
 def get_db():
     with Session(engine) as db:
